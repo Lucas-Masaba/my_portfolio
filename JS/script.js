d601abe718d07655fb3e261550879e144ed74d43
mre55 <misganawmihreteab>
2021-11-09 18:24:18 +0300

    fix: Background blur effect

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,22 @@
 const openMobileMenu = document.querySelectorAll('[data-mobile-menu-target]');
 const closeMobileMenu = document.querySelectorAll('[data-close-button]');
-function openMenu(mobileMenu) {
+const mainTag = document.querySelector('.main');
+
+function openMenu(mobileMenu, mainTag) {
   if (mobileMenu == null) return;
   mobileMenu.classList.add('active');
+  mainTag.classList.add('blur');
 }
 
 function closeMenu(mobileMenu) {
   if (mobileMenu == null) return;
   mobileMenu.classList.remove('active');
+  mainTag.classList.remove('blur');
 }
 openMobileMenu.forEach((button) => {
   button.addEventListener('click', () => {
     const mobileMenu = document.querySelector(button.dataset.mobileMenuTarget);
-    openMenu(mobileMenu);
+    openMenu(mobileMenu, mainTag);
   });
 });
 

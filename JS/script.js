6cb9186bccfb8dac63a07b1f63312a87bed30602
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-09 04:46:26 +0300

    add nav and blur to mobile menu and insert javascript linters

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,24 @@
 const openMobileMenu = document.querySelectorAll('[data-mobile-menu-target]');
 const closeMobileMenu = document.querySelectorAll('[data-close-button]');
-
-openMobileMenu.forEach(button => {
-    button.addEventListener('click', () => {
-        const mobileMenu = document.querySelector(button.dataset.mobileMenuTarget);
-        openMenu(mobileMenu);
-    })
-})
-
-closeMobileMenu.forEach(button => {
-    button.addEventListener('click', () => {
-        const mobileMenu = button.closest('.mobile-menu')
-        closeMenu(mobileMenu);
-    })
-})
-
 function openMenu(mobileMenu) {
-    if(mobileMenu == null) return
-    mobileMenu.classList.add('active');
+  if (mobileMenu == null) return;
+  mobileMenu.classList.add('active');
 }
- 
+
 function closeMenu(mobileMenu) {
-    if(mobileMenu == null) return
-    mobileMenu.classList.remove('active');
+  if (mobileMenu == null) return;
+  mobileMenu.classList.remove('active');
 }
+openMobileMenu.forEach((button) => {
+  button.addEventListener('click', () => {
+    const mobileMenu = document.querySelector(button.dataset.mobileMenuTarget);
+    openMenu(mobileMenu);
+  });
+});
+
+closeMobileMenu.forEach((button) => {
+  button.addEventListener('click', () => {
+    const mobileMenu = button.closest('.mobile-menu');
+    closeMenu(mobileMenu);
+  });
+});

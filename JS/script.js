7c46cf4cb3a333815d992d0f81dc560569135307
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-11 02:11:18 +0300

    added form validation

--- JS/script.js
+++ JS/script.js
@@ -94,46 +94,46 @@ projectDetails.forEach((button) => {
 });
 
 function showMessage(input, message, type) {
-    const msg = input.parentNode.querySelector('small');
-    msg.innerText = message;
-    return type;
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
   }
-  
-  function showError(input, message) {
-    return showMessage(input, message, false);
+  return showSuccess(input);
+}
+
+function validateEmail(input, invalidMsg) {
+  if (!hasValue(input, invalidMsg)) {
+    return false;
   }
-  
-  function showSuccess(input) {
-    return showMessage(input, '', true);
+  const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
   }
-  
-  function hasValue(input, message) {
-    if (input.value.trim() === '') {
-      return showError(input, message);
-    }
-    return showSuccess(input);
+  return true;
+}
+
+const form = document.querySelector('#myForm');
+const EMAIL_INVALID = 'Please enter a correct email address format and uppercase is not allowed!!';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
+  if (emailValid) {
+    form.submit();
   }
-  
-  function validateEmail(input, invalidMsg) {
-    if (!hasValue(input, invalidMsg)) {
-      return false;
-    }
-    const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-  
-    const email = input.value.trim();
-    if (!emailRegex.test(email)) {
-      return showError(input, invalidMsg);
-    }
-    return true;
-  }
-  
-  const form = document.querySelector("#myForm");
-  const EMAIL_INVALID = 'Please enter a correct email address format and uppercase is not allowed!!';
-  
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
-    if (emailValid) {
-      form.submit();
-    }
-  });
+});

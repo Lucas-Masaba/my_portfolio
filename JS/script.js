feae1e8112436773685243265d8ab452dd9380d3
Lucas-Masaba <masabaluke@gmail.com>
2022-10-25 19:12:26 +0300

    clean js a little more

--- JS/script.js
+++ JS/script.js
@@ -308,52 +308,53 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  function showMessage(input, message, type) {
-    const msg = input.parentNode.querySelector('small');
-    msg.innerText = message;
-    return type;
-  }
-
-  function showError(input, message) {
-    return showMessage(input, message, false);
-  }
-
-  function showSuccess(input) {
-    return showMessage(input, '', true);
-  }
-
-  function hasValue(input, message) {
-    if (input.value.trim() === '') {
-      return showError(input, message);
-    }
-    return showSuccess(input);
-  }
-
-  function validateEmail(input, invalidMsg) {
-    if (!hasValue(input, invalidMsg)) {
-      return false;
-    }
-    const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-
-    const email = input.value.trim();
-    if (!emailRegex.test(email)) {
-      return showError(input, invalidMsg);
-    }
-    return true;
-  }
-
-  const form = document.querySelector('#myForm');
-  const EMAIL_INVALID = 'Please enter a correct email address format and uppercase is not allowed!!';
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
-    if (emailValid) {
-      form.submit();
-    }
-  });
-
 });
+
+function showMessage(input, message, type) {
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function validateEmail(input, invalidMsg) {
+  if (!hasValue(input, invalidMsg)) {
+    return false;
+  }
+  const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.querySelector('#myForm');
+const EMAIL_INVALID = 'Please enter a correct email address format and uppercase is not allowed!!';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
+  if (emailValid) {
+    form.submit();
+  }
+});
+
 /* local storage */
 
 /* Javascript object with all data to be stored locally  */
